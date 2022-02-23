9d96fed29528a069c45dd9663418b01e75a29241
KundaPanda <vojdoh@gmail.com>
2022-02-23 19:04:15 +0100

    Week 2

--- app/src/main/java/cz/muni/aqicheck/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/cz/muni/aqicheck/ui/favorites/FavoritesFragment.kt
@@ -1,19 +1,31 @@
 package cz.muni.aqicheck.ui.favorites
 
+import android.content.Context
 import android.os.Bundle
+import android.text.InputType
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import cz.muni.aqicheck.databinding.FragmentFavoritesBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FavoritesFragment : Fragment() {
 
     // TODO 11. FavoritesFragment binding
     private lateinit var binding: FragmentFavoritesBinding
+    private val formatter = SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z", Locale.US)
 
     // TODO 12. onCreateView
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         binding = FragmentFavoritesBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
@@ -27,5 +39,30 @@ class FavoritesFragment : Fragment() {
         // TODO - a spojí předchozí text se získaným časem
         // System.currentTimeMillis()
         // Calendar.getInstance()
+        binding.favoritesButton.setOnClickListener {
+            binding.timeView.text = getTime(binding.timeView.text.toString())
+        }
+        binding.textInput.addTextChangedListener { text ->
+            binding.timeView.text = getTime(text.toString())
+        }
+        binding.textInput.setOnKeyListener { v, keyCode, event ->
+
+            if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                val inputManager: InputMethodManager =
+                    requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                inputManager.hideSoftInputFromWindow(
+                    binding.textInput.windowToken,
+                    InputMethodManager.HIDE_NOT_ALWAYS
+                )
+                binding.textInput.clearFocus()
+                return@setOnKeyListener true
+            }
+            return@setOnKeyListener false
+        }
+    }
+
+    private fun getTime(text: String): String {
+        val date = Date(System.currentTimeMillis())
+        return "$text,\n ${formatter.format(date)}"
     }
 }
